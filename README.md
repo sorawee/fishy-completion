587d79ec2dbd8089f6d7a489bbca2ff93507dd6b
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2020-07-29 14:32:40 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Fishy Completion
+
+This [Quickscript](https://docs.racket-lang.org/quickscript/) script allows users to autocomplete
+an identifier via static analysis. 
+
+To run the script, either use the menu or the keybinding `ctrl ,`. Running repeatedly will cycle through all candidates (similar to the [dynamic completion](https://github.com/Metaxal/quickscript-extra/blob/master/scripts/dynamic-abbrev.rkt) in Quickscript Extra).
+
+## Guarantee and limitation
+
+The script guarantees that any autocompleted identifier at a position will definitely be in scope (according to [Check Syntax](https://docs.racket-lang.org/tools/Check_Syntax.html)), but it does not guarantee that it will be able to recognize every candidate identifier in the scope. For example, identifiers that are imported from other modules, either explicitly (e.g., via `require`) or implicitly (e.g., via `#lang`) are not supported. To autocomplete identifiers from other modules, use the built-in autocomplete feature in DrRacket (`ctrl .`).
+
+In order to run the completion, the program must not have any compile-time error. However, this is too strict because the fact that you intend to use autocompletion probably means that the current program contains invalid identifiers (and thus compile-time errors). Therefore, errors that are caused by the identifier at the current position (i.e., attempting to use non-identifier macro as an identifier macro) and unbound identifiers are tolerated as special cases. Other errors such as unbalanced parentheses or errors in macro expansion will disable the completion.
+
+## Demo
+
+![Demo 1](./demo/demo-fishy-1.gif "Demo 1")
+
+Fishy completion can be used to autocomplete any identifiers that are defined in a module.
+Autocompleted identifiers are guaranteed to be in scope.
+
+![Demo 2](./demo/demo-fishy-2.gif "Demo 2")
+
+Fishy completion recognizes non-apparent bindings such as those generated from the `struct` form. It also recognizes macros. Note that it does not recognize identifiers imported from other modules. Use the built-in autocomplete feature in DrRacket for that case.
+
+![Demo 3](./demo/demo-fishy-3.gif "Demo 3")
+
+Fishy completion can autocomplete quoted identifiers and identifiers that start with a number.
+
+If the current position is not associated with any token that could become an identifier, or if there is no candidate that could be in scope (up to the limitation described above), fishy completion will disable itself. 
+
+![Demo 4](./demo/demo-fishy-4.gif "Demo 4")
+
+When there is a compile-time error, fishy completion will disable itself. There are a few exceptions as described above.
+
---
+++ script.rkt
@@ -0,0 +1,176 @@
+#lang racket/base
+
+(require racket/set
+         racket/class
+         racket/list
+         racket/string
+         racket/format
+         syntax/parse
+         drracket/check-syntax
+         quickscript)
+
+(script-help-string
+ "A proof-of-concept completion with fishy static analysis.")
+
+;; assume that the buffer has number of characters less than `magic-number`
+(define magic-number 1000000000)
+;; assume that each identifier has length less than `magic-number*`
+(define magic-number* 100000)
+(define magic-inkantation 'fishy-completion-SEgTjuZhS1) ; generated from random.org
+
+(define the-submod
+  `(module ,magic-inkantation racket/base
+     (require (for-syntax racket/base))
+     (provide ,magic-inkantation (rename-out [@#%top #%top]))
+     (define-syntax (@#%top stx)
+       (syntax-case stx ()
+         [(_ . x) #'(void)]))
+     (define-syntax (,magic-inkantation stx)
+       (syntax-case stx ()
+         [(_ id ...)
+          (syntax-property
+           #'(void)
+           'disappeared-use
+           (map syntax-local-introduce (syntax->list #'(id ...))))]))))
+
+(define (analyze stx ids)
+  (define vec (list->vector ids))
+  (for/set ([entry (in-list (show-content stx))]
+            #:when (eq? 'syncheck:add-arrow/name-dup/pxpy (vector-ref entry 0))
+            ;; only care about local bindings
+            #:when (not (vector-ref entry 11))
+            ;; syntax-position = 0 is the magic number for our framework
+            #:when (>= (vector-ref entry 5) magic-number))
+    ;; The syntax-span indicates the n-th candidate
+    (~s (syntax-e (vector-ref vec (quotient (- (vector-ref entry 5) magic-number)
+                                            magic-number*))))))
+(define (find-candidates form prefix)
+  (define locals (mutable-set))
+  (define ns (make-base-namespace))
+  (with-handlers ([exn:fail? (λ (_) #f)])
+    (let loop ([stx (parameterize ([current-namespace ns]) (expand form))])
+      (syntax-parse stx
+        [(a . b)
+         (loop #'a)
+         (loop #'b)]
+        [x:id
+         #:when (string-prefix? (~s (syntax-e #'x)) prefix)
+         (set-add! locals (syntax-e #'x))]
+        [_ (void)]))
+    (for/list ([x (in-set locals)]
+               [i (in-naturals)])
+      ;; use syntax-position = 0 and encode the n-th candidate with syntax-span
+      (datum->syntax the-id
+                     x
+                     (list (syntax-source the-id)
+                           1
+                           0
+                           (+ (* magic-number* i) magic-number 1)
+                           0)
+                     the-id))))
+
+(define the-id #f)
+
+(define-syntax-class idable
+  (pattern x:id)
+  ;; number could potentially be an identifier once completed
+  (pattern x:number))
+
+(define (replace top-stx position new-stx)
+  ;; NOTE: #%module-begin's syntax-span is annoying, so let's match
+  ;; against it explicitly.
+  (syntax-parse top-stx
+    [(mod name lang {~and mb-pair (mb . mb-body)})
+     (define mb-body*
+       (let loop ([stx #'mb-body])
+         (syntax-parse stx
+           [() this-syntax]
+           [(a . b) (datum->syntax this-syntax
+                                   (cons (loop #'a) (loop #'b))
+                                   this-syntax
+                                   this-syntax)]
+           [x:idable
+            #:when (and (syntax-source #'x)
+                        (syntax-position #'x)
+                        (syntax-span #'x)
+                        (equal? (syntax-source #'x) (syntax-source top-stx))
+                        (<= (add1 (syntax-position #'x))
+                            position
+                            (+ (syntax-position #'x) (syntax-span #'x))))
+            (set! the-id #'x)
+            (datum->syntax this-syntax new-stx this-syntax this-syntax)]
+           [_ this-syntax])))
+     (datum->syntax
+      this-syntax
+      (list #'mod #'name #'lang (datum->syntax
+                                 #'mb-pair
+                                 (list* #'mb
+                                        the-submod
+                                        `(require ',magic-inkantation)
+                                        mb-body*)
+                                 #'mb-pair
+                                 #'mb-pair))
+      this-syntax
+      this-syntax)]))
+
+(define (my-read s)
+  (with-handlers ([exn:fail? (λ (_) #f)])
+    (parameterize ([read-accept-reader #t])
+      (read-syntax (string->path "dummy") (open-input-string s)))))
+
+(define (query position code-str)
+  (define orig-stx (my-read code-str))
+  (cond
+    [orig-stx
+     (define replaced (replace orig-stx position #t))
+     (cond
+       [the-id
+        (define as-string (~s (syntax-e the-id)))
+        (define as-list (string->list (if (= (+ 2 (string-length as-string))
+                                             (syntax-span the-id))
+                                          (string-append "|" as-string "|")
+                                          as-string)))
+        (define-values (left right)
+          (split-at as-list (- position (syntax-position the-id))))
+        (define left* (list->string left))
+        (define right* (list->string right))
+        (define candidates (find-candidates replaced left*))
+        (values
+         left*
+         right*
+         (cond
+           [candidates
+            (define stx/candidates
+              (replace orig-stx position (cons magic-inkantation candidates)))
+            (sort (set->list (analyze stx/candidates candidates)) string<?)]
+           [else '()]))]
+       [else (values #f #f '())])]
+    [else (values #f #f '())]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Adapted from https://github.com/Metaxal/quickscript-extra/blob/master/scripts/dynamic-abbrev.rkt
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-script fishy-completion
+  #:label "Fishy completion"
+  #:shortcut #\<
+  #:shortcut-prefix (ctl)
+  (λ (_sel #:editor ed)
+    (define pos (send ed get-end-position))
+    (define txt (send ed get-text))
+    (define-values (left right matches)
+      (query (add1 pos) txt))
+    (unless (empty? matches)
+      (define mems (member (string-append left right) matches))
+      (define str
+        (if (and mems (not (empty? (rest mems))))
+            (second mems)
+            (first matches)))
+      (when str
+        (send ed begin-edit-sequence)
+        (send ed delete pos (+ pos (string-length right)))
+        (send ed insert
+              (substring str (string-length left)))
+        (send ed set-position pos)
+        (send ed end-edit-sequence)))
+    #f))
